1a0d239d2fb9159a7ce7cb9005d89d90ddc44948
polygonalcube <polygonalcube@protonmail.com>
2025-04-19 16:06:16 -0400

    Smoothed camera and tweaked character controller.

--- Assets/Scripts/CameraBase.cs
+++ Assets/Scripts/CameraBase.cs
@@ -5,6 +5,9 @@ public class CameraBase : MonoBehaviour
     [SerializeField] Transform playerTarget;
     
     [SerializeField] float zoom = 7.5f;
+    
+    private Vector3 refVelocity = Vector3.zero;
+    [SerializeField] float smoothTime = 0.1f;
 
     private void Start()
     {
@@ -13,12 +16,14 @@ public class CameraBase : MonoBehaviour
 
     void LateUpdate()
     {
-        Vector2 playerPosition = playerTarget.transform.position;
-        Vector2 mousePosition = GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+        Vector3 playerPosition = new(playerTarget.transform.position.x, playerTarget.transform.position.y, -10f);
+        Vector3 mousePosition = GetComponent<Camera>().ScreenToWorldPoint(new(Input.mousePosition.x, 
+            Input.mousePosition.y, -10f));
         
         // Calculating the player position twice for the average keeps the camera closer to the player than the mouse
         // cursor. This prevents the player from being too close to the edges of the screen.
-        transform.position = (playerPosition + playerPosition + mousePosition) / 3f;
-        transform.position += transform.forward * -10f;
+        //transform.position = (playerPosition + playerPosition + mousePosition) / 3f;
+        transform.position = Vector3.SmoothDamp(transform.position, 
+            (playerPosition + playerPosition + mousePosition) / 3f, ref refVelocity, smoothTime);
     }
 }
